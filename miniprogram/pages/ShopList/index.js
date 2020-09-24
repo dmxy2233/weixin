b4f288dcef8e74278aeef8ffbb04f931828969d0
unknown <unknown@example.com>
2020-09-24 20:21:14 +0800

    Initial Commit

---
+++ miniprogram/pages/ShopList/index.js
@@ -0,0 +1,12 @@
+
+Page({
+  data: {
+    ShopList: [
+      {img:"../ShopList/img/fanqiechaodan.png",message: '番茄炒蛋'}, 
+      {img:"../ShopList/img/fanqiechaodan.png",message: '黄瓜炒蛋' },
+      {img:"../ShopList/img/fanqiechaodan.png",message: '蛋炒饭' },
+      {img:"../ShopList/img/fanqiechaodan.png",message: '番茄炒西红柿'},
+      {img:"../ShopList/img/fanqiechaodan.png",message: '青椒炒菜' },
+    ]
+  }
+})
